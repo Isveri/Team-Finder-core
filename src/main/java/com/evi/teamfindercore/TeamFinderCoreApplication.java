14a93bd6eec8882cb8de57aed7b97cf648b20ae8
Evistix <mephistakaiwan@gmail.com>
2023-06-06 21:07:01 +0200

    initial + BOM

---
+++ src/main/java/com/evi/teamfindercore/TeamFinderCoreApplication.java
@@ -0,0 +1,13 @@
+package com.evi.teamfindercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFinderCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamFinderCoreApplication.class, args);
+    }
+
+}
