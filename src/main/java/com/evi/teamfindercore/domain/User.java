73ad7af0b6ab1a30aba76842288c7cf268e479a4
Evistix <mephistakaiwan@gmail.com>
2023-08-01 19:01:24 +0200

    WebSocket refactor / Group room management fix

--- src/main/java/com/evi/teamfindercore/domain/User.java
+++ src/main/java/com/evi/teamfindercore/domain/User.java
@@ -33,7 +33,6 @@ public class User implements UserDetails, CredentialsContainer {
     @NotBlank
     private String password;
 
-    // @NotBlank
     private String email;
 
     private String name;
